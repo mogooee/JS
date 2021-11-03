8dbc68bfbf7a12457f4d25ffe95fcfe4d8ece774
mogooee <mogoo22@naver.com>
2021-11-04 05:09:19 +0900

    n진수, 순서p 추가

--- Day2/Day2_1.js
+++ Day2/Day2_1.js
@@ -1,33 +1,59 @@
-function solution1(n,t,m){ //모두 출력 
+function solution1(n1,t1,m1,p){ //모두 출력 
 
-    this.num = t*m; // this 전역변수 
+    this.n1=n1;
+    this.t1=t1;
+    this.m1=m1;
+    this.p=p;
+    num1 = t1*m1; // this 전역변수 
     let arr=[];
     
-     for(i=0;i<num;i++) {
-     arr += i.toString(n);  //n진수로 변환 
+     for(i=0;i<num1;i++) {
+     arr += i.toString(n1);  //n진수로 변환 
      }
      
     this.data=arr.split(""); //split함수 
-    console.log(data)
-    return data;
+    console.log(`모두 출력 : ${data}`)
+
+    solution3(m1,p)
 }
-    
-function solution2(){
- 
-    let arr_a="";
-    let arr_b="";
 
-    for(i=0;i<num;i++){
+function solution3(m1,p){ //n명일 때 
 
-        if(i%2===0) //A
-        arr_a+=data[i];
+   
+    let arr_p=[];
+    let p_i;
+    let count=0;
 
-        else //B
-        arr_b+=data[i];
+    for(i=0;i<data.length;i++){
+        p_i=(p-1)+(i*m1); 
+
+        if(count<t1) //undefined 제거 
+        arr_p.push(data[p_i]);
+
+        count++;
     } 
-    console.log(arr_a)
-    console.log(arr_b)
+    console.log(`${p}번째 : ${arr_p}`)
+    return arr_p;
 }
 
-solution1(2,4,2)
-solution2(2,4,2)   
+function solution2(n2,t2,m2){ //2명일 때
+
+    let num2=t2*m2;
+    let arr=[];
+    let arr_a=[];
+    let arr_b=[];
+
+    for(i=0;i<num2;i++){
+        arr += i.toString(n2);  //n진수로 변환 
+        if(i%2===0)
+        arr_a.push(arr[i]);
+        else
+        arr_b.push(arr[i]);
+     } 
+    data2=arr.split("");
+        
+    console.log(`\nA: ${arr_a} , B: ${arr_b}`)
+   }
+
+solution1(10,6,3,2)
+solution2(2,5,2)  
