1b04c9923d3ffa9bb2b091a73dc6c0f70073bce9
mogooee <mogoo22@naver.com>
2021-11-03 15:48:29 +0900

    함수 호출 순서, 값 출력함수

--- Day1.js
+++ Day1.js
@@ -1,50 +1,81 @@
-function getArea(figure){
-    switch(figure){
+let shape=[];
+let data=[];
 
+function getArea(figure){ //function 선언문  
+
+    switch(figure){  //arguments[0] = figure
+  
         case 'rect':
-            return Rect(arguments[1],arguments[2]);
+            return getRect(arguments[1],arguments[2]);  //a,b
+            
 
         case 'trapezoid':
-            return Trapezoid(arguments[1],arguments[2],arguments[3]);
+            return getTrapezoid(arguments[1],arguments[2],arguments[3]); //a,b,c
 
         case 'circle':
-            return Circle(arguments[1],arguments[2]);
+            return getCircle(arguments[1],arguments[2]); //a,b
 
         default:
             break; 
+    }  
+ 
+}
+
+  //넓이 구하는 함수   
+
+function getRect(length,width){ //사각형 넓이 함수
+    shape.push('rect');
+    data.push(length*width);
+    return length*width; 
+
+}
+
+function getTrapezoid(length,width,height){ //사다리꼴 넓이 함수
+    shape.push('Trapezoid');
+    data.push(((length+width)*height)/2);
+    return ((length+width)*height)/2;
+}
+
+function getCircle(r,n){ //원의 넓이 함수 
+    shape.push('circle');
+   
+    if(n===undefined){ // 1.반지름값만 입력
+    data.push(r*r* Math.PI);
+    return r*r* Math.PI;
     }
-}; 
 
-function Rect(a,b){
-    return a*b; 
-}
-
-function Trapezoid(a,b,c){
-    return ((a+b)*c)/2;
-}
-
-function Circle(a,b){
-    
-    if(b===undefined)
-    return a*a* Math.PI;
-
-    else{
+    else{ //2.반지름, b까지 1씩 반지름 길이 증가한 원 넓이의 총합 
 
         let result=0;
 
-        while(a<=b)
+        while(r<=n) 
         {
-        result += a*a*Math.PI;  
-        a++;  
+        result += r*r*Math.PI;  
+        r++;  
         }
 
+        data.push(result);
         return result;
     }
-
-
 }
 
-console.log (getArea('rect',10,15));
-console.log (getArea('trapezoid',10,15,12));
-console.log (getArea('circle',10));
-console.log (getArea('circle',1,3));
+function printExecutionSequence(){  //함수 호출 순서, 값 출력 
+
+    console.log('계산수행순서 :');
+
+        for(i=0; i<=(shape.length)-1;i++){ 
+
+            console.log(`${shape[i]}, ${data[i]}`); //템플릿 문자열
+
+
+        }
+}
+    
+
+    getCircle(2) 
+    getRect(2,3)
+    getTrapezoid(10,15,12); 
+    getArea('circle',2) 
+    getArea('rect',2,3) 
+    getArea('trapezoid',10,15,12)
+    printExecutionSequence()
