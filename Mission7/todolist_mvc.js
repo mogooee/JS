6351077cbc244644f7f1a5451ac98670a6dd9221
mogooee <mogoo22@naver.com>
2021-11-25 12:16:58 +0900

    distructuring

--- Mission7/todolist_mvc.js
+++ Mission7/todolist_mvc.js
@@ -10,7 +10,7 @@ class Model {
     this.LIST = [];
     this.id = 0;
   }
-  addLIST() {
+  AddLIST() {
     const toDo = this.selectors.input.value;
 
     if (toDo) {
@@ -19,6 +19,7 @@ class Model {
       this.selectors.input.value = "";
     }
   }
+  EditToDo(element) {}
 }
 
 class View {
@@ -30,7 +31,7 @@ class View {
     this.LINE_THROUGH = "lineThrough";
   }
 
-  showLIST({ name, id, done, trash }) {
+  ShowLIST({ name, id, done, trash }) {
     //const { name, id, done, trash } = toDo;
 
     if (trash) {
@@ -46,7 +47,7 @@ class View {
     </li> 
     `;
     const position = "beforeend";
-    //공백 입력시에는 보여주지 않는다.
+
     if (name) {
       list.insertAdjacentHTML(position, item);
     }
@@ -58,10 +59,6 @@ class View {
     element.parentNode
       .querySelector(".text")
       .classList.toggle(this.LINE_THROUGH);
-
-    //model = id, view=done,trash
-    console.log(this.model.LIST);
-    console.log(element.id);
 
     this.model.LIST[element.id].done = this.model.LIST[element.id].done
       ? false
@@ -81,38 +78,37 @@ class Controller {
   }
 
   init() {
-    //이벤트
-    this.pushEnter();
-    this.clickAddToDo();
-    this.clickDoneOrTrash();
+    this.EnterAddToDo();
+    this.ClickAddToDo();
+    this.ClickDoneOrTrash();
   }
 
-  pushEnter() {
+  EnterAddToDo() {
     document.addEventListener("keyup", (event) => {
       if (event.keyCode === 13) {
-        this.model.addLIST();
-        this.view.showLIST(this.model.LIST[this.model.id - 1]);
+        this.model.AddLIST();
+        this.view.ShowLIST(this.model.LIST[this.model.id - 1]);
       }
     });
   }
 
-  clickAddToDo() {
+  ClickAddToDo() {
     const add_item = document.querySelector(".add_item");
 
     add_item.addEventListener("click", (event) => {
       const element = event.target;
-      if (element.nodeName === "I") {
-        this.model.addLIST();
-        this.view.showLIST(this.model.LIST[this.model.id - 1]);
+      if (element.nodeName !== "I") {
+        return;
       }
+      this.model.AddLIST();
+      this.view.ShowLIST(this.model.LIST[this.model.id - 1]);
     });
   }
 
-  clickDoneOrTrash() {
+  ClickDoneOrTrash() {
     document.getElementById("list").addEventListener("click", (event) => {
       event.preventDefault();
       const element = event.target;
-      //아이콘이 아니면 종료
       if (element.nodeName !== "I") {
         return;
       }
@@ -123,6 +119,8 @@ class Controller {
         this.view.CompleteToDo(element);
       } else if (elementJob === "delete") {
         this.view.RemoveToDo(element);
+      } else if (elementJob === "edit") {
+        this.model.EditToDo(element);
       }
     });
   }
