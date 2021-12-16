448c393fe0e1e195f1e5af5c291808e828a05b0f
mogooee <mogoo22@naver.com>
2021-12-16 23:59:29 +0900

    Refact: 호출함수를 저장하는 함수, 출력하는 함수 분리

--- Mission1/getArea.js
+++ Mission1/getArea.js
@@ -1,3 +1,5 @@
+let executionFunction = [];
+
 const getArea = (figure, ...num) => {
   switch (figure) {
     case "circle":
@@ -10,6 +12,7 @@ const getArea = (figure, ...num) => {
 };
 
 const getCircle = (num) => {
+  saveExecutionSequence("circle");
   if (num[1]) {
     let sum = 0;
     for (let i = num[0]; i <= num[1]; i++) {
@@ -20,11 +23,22 @@ const getCircle = (num) => {
 };
 
 const getRect = (num) => {
+  saveExecutionSequence("rect");
   return num[0] * num[1];
 };
 
 const getTrapezoid = (num) => {
+  saveExecutionSequence("trapezoid");
   return ((num[0] + num[1]) * num[2]) / 2;
+};
+
+const saveExecutionSequence = (figure) => {
+  executionFunction.push(figure);
+  return;
+};
+
+const printExecutionSequence = () => {
+  return `계산수행순서: ${executionFunction}`;
 };
 
 console.log(getCircle(2));
@@ -32,3 +46,5 @@ console.log(getArea("circle", 10));
 console.log(getArea("rect", 10, 15));
 console.log(getArea("trapezoid", 10, 15, 12));
 console.log(getArea("circle", 1, 2));
+
+console.log(printExecutionSequence());
