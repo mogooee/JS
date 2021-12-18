9e252faa16f65becd2de0149a3ea071fa1b75f71
mogooee <mogoo22@naver.com>
2021-12-19 02:05:54 +0900

    Rename, Refact: 객체 메서드, filter 함수

---
+++ Mission2/RadixGame/filterId.js
@@ -0,0 +1,20 @@
+const peoples = ["crong!@#", "honux5", "sarah#", "hea3d", "zello", "5lucas"];
+
+const filterId = {
+  nonSpecialChar(peoples) {
+    return peoples.filter((e) => {
+      return !e.match(/[~!@#$%^&*()_+{}]+/gi);
+    });
+  },
+  nonNumChar(peoples) {
+    let arr = [];
+    peoples.filter((e) => {
+      arr.push(e.replace(/[0-9]/, ""));
+    });
+    return arr;
+  },
+};
+
+console.log(filterId.nonSpecialChar(peoples));
+const filterNonSpecialChar = filterId.nonSpecialChar(peoples);
+console.log(filterId.nonNumChar(filterNonSpecialChar));
